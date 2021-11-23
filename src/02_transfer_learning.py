9098f8e42ff9a97f750ae99fd9c5f0ab7d5b2ca8
abhishekvarma12345 <abhishekvarmad@gmail.com>
2021-11-23 18:17:13 +0530

    transfer model applied for even and odd digit

--- src/02_transfer_learning.py
+++ src/02_transfer_learning.py
@@ -5,9 +5,10 @@ from tqdm import tqdm
 import logging
 from src.utils.common import read_yaml, create_directories
 import tensorflow as tf
+import io
 
 
-STAGE = "basic model" ## <<< change stage name 
+STAGE = "transfer model" ## <<< change stage name 
 
 logging.basicConfig(
     filename=os.path.join("logs", 'running_logs.log'), 
@@ -16,6 +17,11 @@ logging.basicConfig(
     filemode="a"
     )
 
+def even_odd_label(y_train, y_test, y_valid):
+    y_train_bin = np.where(y_train % 2 == 0, 1, 0)
+    y_test_bin = np.where(y_test % 2 == 0, 1, 0)
+    y_valid_bin = np.where(y_valid % 2 == 0, 1, 0)
+    return (y_train_bin, y_test_bin, y_valid_bin)
 
 def main(config_path):
     ## read config files
@@ -27,48 +33,69 @@ def main(config_path):
     X_test = X_test / 255.0
     X_valid, X_train = X_train_full[:5000], X_train_full[5000:]
     y_valid, y_train = y_train_full[:5000], y_train_full[5000:]
-
+    y_train_bin, y_test_bin, y_valid_bin = even_odd_label(y_train, y_test, y_valid)
     ## set the seeds
     seed = 2021 ## get it from config
     tf.random.set_seed(seed)
     np.random.seed(seed)
 
-    ## define layers
-    LAYERS = [
-        tf.keras.layers.Flatten(input_shape=(28, 28)),
-        tf.keras.layers.Dense(300),
-        tf.keras.layers.LeakyReLU(), ## alternate way
-        tf.keras.layers.Dense(100),
-        tf.keras.layers.LeakyReLU(),
-        tf.keras.layers.Dense(10, activation="softmax")
-    ]
+    model_file_path = os.path.join("artifacts","models","base_model.h5")
+    ## load base model
+    base_model = tf.keras.models.load_model(model_file_path)
 
-    ## define model
-    model = tf.keras.models.Sequential(LAYERS)
+    # log our model summary info in logs
+    def _log_model_summary(model):
+        with io.StringIO() as stream:
+            model.summary(print_fn= lambda x: stream.write(f"{x}\n"))
+            summary_string = stream.getvalue()
+        return summary_string
 
-    LOSS = "sparse_categorical_crossentropy"
+    ## model summary
+    logging.info(f"base model summary: \n{_log_model_summary(base_model)}")
+
+    base_model = base_model.layers[:-1]
+    ## define transfer model
+    for layer in base_model:
+        layer.trainable = False
+    
+    base_model.append(tf.keras.layers.Dense(1, activation="sigmoid", name="output_layer"))
+    
+    transfer_model = tf.keras.models.Sequential(base_model)
+
+    LOSS = "binary_crossentropy"
     OPTIMIZER = tf.keras.optimizers.SGD(learning_rate=1e-3)
     METRICS = ["accuracy"]
 
     ## compiling model
-    model.compile(loss=LOSS, optimizer=OPTIMIZER, metrics=METRICS)
+    transfer_model.compile(loss=LOSS, optimizer=OPTIMIZER, metrics=METRICS)
+
+
+    # log our model summary info in logs
+    def _log_model_summary(model):
+        with io.StringIO() as stream:
+            model.summary(print_fn= lambda x: stream.write(f"{x}\n"))
+            summary_string = stream.getvalue()
+        return summary_string
+
+    ## model summary
+    logging.info(f"{STAGE} summary: \n{_log_model_summary(transfer_model)}")
 
     # training model
-    model.fit(X_train, y_train, 
+    transfer_model.fit(X_train, y_train_bin, 
     epochs=10,
-    validation_data=(X_valid, y_valid),
+    validation_data=(X_valid, y_valid_bin),
     verbose=2)
 
     # save the base model
     model_dir_path = os.path.join("artifacts", "models")
     create_directories([model_dir_path])
 
-    model_file_path = os.path.join(model_dir_path, "base_model.h5")
+    model_file_path = os.path.join(model_dir_path, "transfer_model.h5")
 
-    model.save(model_file_path)
+    transfer_model.save(model_file_path)
 
-    logging.info(f"base model saved at {model_file_path}")
-    logging.info(f"evaluation metrics {model.evaluate(X_test, y_test)}")
+    logging.info(f"{STAGE} saved at {model_file_path}")
+    logging.info(f"{STAGE} evaluation metrics {transfer_model.evaluate(X_test, y_test_bin)}")
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
